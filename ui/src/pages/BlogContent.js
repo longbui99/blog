dfdf5ddaadc94a3e9d70b5d5931af20c811c09c7
longbui99 <philonglml9@gmail.com>
2024-11-02 00:26:43 +0700

    REL

--- ui/src/pages/BlogContent.js
+++ ui/src/pages/BlogContent.js
@@ -389,7 +389,6 @@ function BlogContent({ updateMainContentEditableContent, isLoggedIn, routes, onC
                                     <line x1="12" y1="5" x2="12" y2="19"></line>
                                     <line x1="5" y1="12" x2="19" y2="12"></line>
                                 </svg>
-                                <span className="button-text">Create</span>
                             </button>
                             <button 
                                 onClick={handleEditToggle} 
@@ -409,7 +408,6 @@ function BlogContent({ updateMainContentEditableContent, isLoggedIn, routes, onC
                                     <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
                                     <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
                                 </svg>
-                                <span className="button-text">{isEditing ? 'Exit' : 'Edit'}</span>
                             </button>
                             
                             <button 
@@ -434,7 +432,6 @@ function BlogContent({ updateMainContentEditableContent, isLoggedIn, routes, onC
                                     <line x1="10" y1="11" x2="10" y2="17"></line>
                                     <line x1="14" y1="11" x2="14" y2="17"></line>
                                 </svg>
-                                <span className="button-text">Delete</span>
                             </button>
 
                             {/* Publish Button as a boolean toggle */}
@@ -456,7 +453,6 @@ function BlogContent({ updateMainContentEditableContent, isLoggedIn, routes, onC
                                 >
                                     <path d={isPublished ? "M12 2L2 12h3v8h8v-3h3L12 2z" : "M12 2L2 12h3v8h8v-3h3L12 2z"} />
                                 </svg>
-                                <span className="button-text">{isPublished ? 'Published' : 'Publish'}</span>
                             </button>
 
                             {isEditing && (
