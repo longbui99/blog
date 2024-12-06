3ade13fa6d9c38c9d427ae898055fc56d2f46bf7
longbui99 <philonglml9@gmail.com>
2024-12-06 17:21:23 +0700

    Revert "[REL]"
    This reverts commit 1aea88df00fa349f15fe473a084972347d3966ff.

--- ui/src/utils/contentParser.js
+++ ui/src/utils/contentParser.js
@@ -48,11 +48,6 @@ function processNode(node, currentRoute) {
       return <CodeBlock key={Math.random()} code={code.textContent.trim()} language={language} />;
     }
 
-    if (tagName === 'pre') {
-      const language = getLanguage(code.className);
-      return <CodeBlock key={Math.random()} code={node.textContent.trim()} language={language} />;
-    }
-
     if (tagName === 'code') {
       const language = getLanguage(node.className);
       return <CodeBlock key={Math.random()} code={node.textContent.trim()} language={language} inline />;
