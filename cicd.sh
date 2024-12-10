3d98172d3790cfba8270f1b37eee357dfa085973
longbui99 <philonglml9@gmail.com>
2024-12-10 11:37:31 +0700

    [REL] CICD

--- cicd.sh
+++ cicd.sh
@@ -2,4 +2,5 @@
 set -x
 sudo git pull origin master
 cd ui && npm install && npm run build
+cd ../webserver && source venv/bin/activate && pip3 install -r requirements
 sudo systemctl restart blogserver
