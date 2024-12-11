ca64550fe544c0cde99fbdcea8f731709a877858
longbui99 <philonglml9@gmail.com>
2024-12-11 11:19:49 +0700

    [REL]

--- deploy/cicd.sh
+++ deploy/cicd.sh
@@ -3,7 +3,4 @@ set -x
 cd ../
 sudo git pull origin master
 cd ui && npm install && npm run build
-docker-compose restart
-cd ../
-cd webserver && source venv/bin/activate && pip3 install -r requirements
 sudo systemctl restart blogserver
